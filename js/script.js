2e6ee2ab8182989ba4bcda1d0702cf5bb729255b
Davide <kalelc351k@gmail.com>
2020-10-29 17:46:49 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -7,151 +7,74 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-// array inizialmente vuota
-// var arrayNumeri = [];
+
+// ====================================================================================
+
+// // input numeri generati random da pc e controllati che non siano uguali
+// var arrayRandom = [];
+// for (var i = 0; i < 22; i++) {
+//   var numeriRandom = Math.floor(Math.random() * 100) + 1;
+//   console.log('log di numeri random: ' + numeriRandom);
+//   var uguale = arrayRandom.includes(numeriRandom);
+//   if (uguale == true) {
+//     alert('numero doppio trovato');
+//   } else if (uguale == false && arrayRandom.length < 16) {
+//     arrayRandom.push(numeriRandom);
+//   }
+// }
+// console.log(arrayRandom);
 //
-// // compilazione array vuota con 16 numeri random da 1 a 100
+// // =======================================================================================
+//
+// // input numeri utente 16 volte controllando che non siano ripetuti
+// var arrayUtente = [];
 // for (var i = 0; i < 16; i++) {
-//   var randomNumeri = Math.floor(Math.random() * 100 + 1);
-//   console.log('numero random: ' + randomNumeri);
-//   arrayNumeri.push(randomNumeri);
-// }
-// console.log('composizione intera array: ' + arrayNumeri);
-// console.log(arrayNumeri[0]);
-// console.log(arrayNumeri[1]);
-
-// var randomArray = [];
-// for (var i = 0; i < 16; i++) {
-//   var numeriRandom = Math.floor(Math.random() * 100) +1;
-//   var x = true;
-//   for (var j = 0; j < i; j++) {
-//     if (randomArray[j] == numeriRandom) {
-//     x = false;
+//   var numeriUtente = parseInt(prompt('Inserisci 16 numeri da 1 a 100'));
+//   console.log('log di numeri utente: ' + numeriUtente);
+//   var ugualeU = arrayUtente.includes(numeriUtente);
+//   if (ugualeU == true) {
+//     alert('Non puoi inserire numeri uguali');
+//   } else if (ugualeU == false && arrayUtente.length < 16) {
+//     arrayUtente.push(numeriUtente);
+//     // confronto tra Array
+//     for (var z = 0; z < arrayRandom.length; z++) {
+//       var ugualex = arrayRandom.includes(arrayUtente[z]);
 //     }
-//   }
-//   if (x) {
-//     randomArray[i] = numeriRandom;
-//   } else {
-//     console.log('numero uguale trovato');
-//     i--;
-//   }
-// }
-// console.log('log di randomA ' + randomArray);
-// console.log('log di tmp ' + numeriRandom);
-// console.log('log di randoma ' + randomArray);
-
-// input singolo  prova
-// var inputUtente1 = parseInt(prompt('Inserisci 16 numeri alla volta da 1 a 100'));
-// arrayUtente.push(inputUtente1);
-// console.log(arrayUtente);
-// // richiesta input tramite prompt per 15 volte
-// for (var y = 0; y < 15; y++) {
-//   var inputUtente = parseInt(prompt('Inserisci 16 numeri alla volta da 1 a 100'));
-//   if (inputUtente != arrayUtente[y]) {
-//     console.log('controllo y ' + arrayUtente[y]);
-//     arrayUtente.push(inputUtente);
-//     console.log(arrayUtente);
-//   } else if (inputUtente == arrayUtente[y]) {
-//     alert('Non puoi inserire numeri uguali')
+//     console.log('Mi dispiace hai perso, hai trovato una mina' + ugualex);
 //   }
 // }
 // console.log(arrayUtente);
 
-// // array
-// var arrayUtente = [4, 6, 7, 9, 11, 34];
-// console.log(arrayUtente);
-// // for (var i = 0; i < 16; i++) {
-// // }
-// var inputUtente = parseInt(prompt('Inserisci 16 numeri alla volta da 1 a 100'));
-// // var uguale = arrayUtente.includes(inputUtente);
-// console.log('esiste ' + arrayUtente.includes(inputUtente));
-// // if (uguale == true) {
-// //   alert('numero uguale trovato');
-// // }
 
-// ---------------------------------------------------------------------------------
-
-// input numeri generati random da pc
-var arrayRandom = [];
+var utenteArray = [];
 for (var i = 0; i < 16; i++) {
-  var numeriRandom = Math.floor(Math.random() * 100) + 1;
-  console.log('log di numeri random: ' + numeriRandom);
-  var uguale = arrayRandom.includes(numeriRandom);
-  if (uguale == true) {
-    alert('numero doppio trovato');
+  var numeriUtente = parseInt(prompt('Inserisci 16 numeri da 1 a 100'));
+  var x = true;
+  for (var j = 0; j < i; j++) {
+    if (utenteArray[j] == numeriUtente) {
+    x = false;
+    }
+  }
+  if (x == true) {
+    utenteArray[i] = numeriUtente;
   } else {
-    arrayRandom.push(numeriRandom);
+    console.log('numero uguale trovato');
+    i--;
   }
 }
-console.log(arrayRandom);
-
-// input numeri inseriti e controllati se presenti
-// var arrayUtente = [];
-// for (var i = 0; i < 16; i++) {
-//   var numeriUtente = parseInt(prompt('Inserisci un numero 16 numeri da 1 a 100'))
-//   console.log('log di numeri random: ' + numeriUtente);
-//   var uguale = arrayUtente.includes(numeriUtente);
-//   if (uguale == true) {
-//     alert('numero doppio trovato');
-//   } else {
-//     arrayUtente.push(numeriUtente);
-//   }
-// }
-
-// condizione che definisce quando l'array e' inferiore a 16
-// if (arrayUtente.length == 16) {
-//   var componenti16 = true;
-// } else {
-//   componenti16 = false;
-// }
-
-// while (arrayUtente.length > 16) {
-//   numeriUtente = parseInt(prompt('---------------------------------Inserisci un numero 16 numeri da 1 a 100'));
-//   arrayUtente.push(numeriUtente);
-// }
+console.log('log di arrayUtente ' + utenteArray);
+console.log('log di ultimo numeriUtente ' + numeriUtente);
 
 
-// var i = 0;
-// while (i < 16) {
-//   var numeriUtente = parseInt(prompt('Inserisci un numero 16 numeri da 1 a 100'))
-//   console.log('log di numeri random: ' + numeriUtente);
-//   var uguale = arrayUtente.includes(numeriUtente);
-//   if (uguale == true) {
-//     alert('numero doppio trovato');
-//   } else {
-//     arrayUtente.push(numeriUtente);
-//   }
-//   i++;
-// }
-// if (arrayUtente.length == 16) {
-//   var stop = true;
-// }
 
 
-// ----------------------------------------
-// var arrayUtente = [];
-// var i = 0;
-// do {
-//   var numeriUtente = parseInt(prompt('Inserisci un numero 16 numeri da 1 a 100'))
-//   console.log('log di numeri random: ' + numeriUtente);
-//   var uguale = arrayUtente.includes(numeriUtente);
-//   if (uguale == true) {
-//     alert('numero doppio trovato');
-//   } else {
-//     arrayUtente.push(numeriUtente);
-//   }
-//   i++;
-// } while (arrayUtente.length == 16)
-//
-// console.log('componenti dentro la array', arrayUtente.length);
-
-// if (componenti16 == false) {
-//   numeriUtente = parseInt(prompt('---------------------------------Inserisci un numero 16 numeri da 1 a 100'));
-//   arrayUtente.push(numeriUtente);
-// }
 
 
-// console.log(arrayUtente);
+
+
+
+
+
 
 
 // BONUS: (da fare solo se funziona tutto il resto)
